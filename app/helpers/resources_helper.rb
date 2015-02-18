a4bbaa2db680bfb0216082e407e572417020fa45
Oliver Garcia <ogarci5@gmail.com>
2015-02-17 23:42:02 -0600

    Updated centauri, more semantic, more icons, more pretty

--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -1,7 +1,9 @@
 module ResourcesHelper
 
   def toggle_groups_button(toggle)
-    button_tag('Toggle Groups', {id: 'toggle-groups', class: "btn btn-default #{toggle ? 'active' : ''}",
-                                 autocomplete: 'off', :'data-toggle' => 'button',  :'aria-pressed' => !toggle})
+    button_tag(id: 'toggle-groups', class: "btn btn-default #{toggle ? 'active' : ''}",
+                                 autocomplete: 'off', :'data-toggle' => 'button',  :'aria-pressed' => !toggle) do
+      'Toggle Groups  <span class="glyphicon glyphicon-expand"></span>'.html_safe
+    end
   end
 end
